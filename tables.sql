759b40fdf7d8f88f96089b828c117cd7668d95e4
heusyfelype <felype.heusy@gmail.com>
2022-06-08 16:21:10 -0300

    feat: deploy funcinoando e tabelas criadas no bd

---
+++ tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE "users" (
+	"id" serial  NOT NULL PRIMARY KEY,
+	"name" TEXT NOT NULL,
+	"email" TEXT NOT NULL UNIQUE,
+	"password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP DEFAULT NOW()
+)
+
+CREATE TABLE "links" (
+	"id" serial NOT NULL PRIMARY KEY,
+	"linkOriginal" TEXT NOT NULL,
+	"linkEncurtado" TEXT NOT NULL UNIQUE,
+	"acessos" integer NOT NULL,
+	"userId" integer NOT NULL REFERENCES "users"("id"),
+    "createdAt" TIMESTAMP DEFAULT NOW()
+) 
+
+CREATE TABLE "autorizacao" (
+	"id" serial NOT NULL PRIMARY KEY,
+	"userId" integer NOT NULL,
+	"token" TEXT NOT NULL,
+    "createdAt" TIMESTAMP DEFAULT NOW()
+)

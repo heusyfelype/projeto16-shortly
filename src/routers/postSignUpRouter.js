109ddf301ec20493c665983ac063a4df25249a57
heusyfelype <felype.heusy@gmail.com>
2022-06-09 14:28:07 -0300

    fix: router signup

--- src/routers/postSignUpRouter.js
+++ src/routers/postSignUpRouter.js
@@ -2,9 +2,10 @@ import { Router } from "express";
 
 
 import { validPostSignUp } from "../middlewares/validPostSignUp.js";
+import { signUpcontroller } from "../controllers/signUpcontroller.js";
 
 const postSignUpRouter = Router();
 
-postSignUpRouter.post('/signup', validPostSignUp)
+postSignUpRouter.post('/signup', validPostSignUp, signUpcontroller);
 
 export default postSignUpRouter

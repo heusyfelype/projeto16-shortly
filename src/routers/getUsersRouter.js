74e27fd1207a3c0a8b1dc128c32232557bf249e3
heusyfelype <felype.heusy@gmail.com>
2022-06-10 13:32:17 -0300

    fix: importação em getUsersRouter

--- src/routers/getUsersRouter.js
+++ src/routers/getUsersRouter.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 
-import { validGetUsers } from "../middlewares/validGetUsers.js";
 import { getUsersController } from "../controllers/getUsersController.js";
 import {validAuthorization} from "../middlewares/validAuthorization.js";
 
